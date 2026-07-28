65aaa64e21892d786589104e796100731baea58b
Humberto <gigantedeluz@gmail.com>
2026-07-27 21:59:04 -0300

    fix: optmizing seo and creating map files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import type { Metadata } from "next";
 import { AuthorityMetrics } from "@/components/home/AuthorityMetrics";
 import { ClientsSection } from "@/components/home/ClientsSection";
 import { TestimonialHero } from "@/components/home/TestimonialHero";
 import { Footer } from "@/components/layout/Footer/Footer";
 import { Header } from "@/components/layout/Header/Header";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "/",
+  },
+};
 
 export default function HomePage() {
   return (
